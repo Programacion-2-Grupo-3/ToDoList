d6942191af39cf2791623ee74fd9a26dfd25042b
AdelaGaray <adela_garay@unitec.edu>
2024-09-18 22:47:49 -0600

    Actualizacion AddTareaForm.xaml.cs

--- Views/AddTareaForm.xaml.cs
+++ Views/AddTareaForm.xaml.cs
@@ -1,9 +1,24 @@
+using ToDoList.Models;
+using ToDoList.ViewModels;
+
 namespace ToDoList.Views;
 
 public partial class AddTareaForm : ContentPage
 {
+	private AddTareaFormViewModel ViewModel;
+
 	public AddTareaForm()
 	{
+        InitializeComponent();
+		ViewModel = new AddTareaFormViewModel();
+		BindingContext = ViewModel;
+	
+	}
+	public AddTareaForm(Tarea tarea)
+	{
 		InitializeComponent();
+		ViewModel = new AddTareaFormViewModel(tarea);
+		BindingContext = ViewModel;
+
 	}
 }
