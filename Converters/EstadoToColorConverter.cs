2862be2cd3ca40d0c2a6cb2f827ed01c3d543289
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-09-18 22:46:47 -0600

    Actualización del EstadoToColorConverter.cs
    Se añadió color a cada Estado de la tarea.

--- Converters/EstadoToColorConverter.cs
+++ Converters/EstadoToColorConverter.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.Maui.Controls;
 
 namespace ToDoList.Converters
 {
-    internal class EstadoToColorConverter
+    public class EstadoToColorConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string estado)
+            {
+                return estado switch
+                {
+                    "Terminada" => Colors.Green,   // Verde para tareas finalizadas
+                    "En proceso" => Colors.Blue, // Azul para tareas en progreso
+                    "Por hacer" => Colors.Red,      // Rojo para tareas por hacer
+                    _ => Colors.Gray,               // Color gris para estados desconocidos
+                };
+            }
+            return Colors.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
